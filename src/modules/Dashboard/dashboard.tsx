6aa11c9d179f06f3e53c09ac3c579a4c337708bc
jamesngdev <jamesngdev@gmail.com>
2023-08-07 16:32:18 +0700

    Add alias and remove .env

--- src/modules/Dashboard/dashboard.tsx
+++ src/modules/Dashboard/dashboard.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import FullPageLoading from "@/components/FullPageLoading";
 
 function Dashboard() {
   return (
     <div>
-      <h1>Dashboard page</h1>
+      <FullPageLoading />
     </div>
   );
 }
