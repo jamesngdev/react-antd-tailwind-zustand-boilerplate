f9529f63936d950304295b97428025e061dab0b5
jamesngdev <jamesngdev@gmail.com>
2023-08-09 18:26:30 +0700

    Add menu manager

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,47 +1,9 @@
 import React, { useState } from 'react';
-import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Layout, theme } from 'antd';
 import { Outlet } from 'react-router-dom';
+import Menu from './menu';
 
 const { Header, Content, Footer, Sider } = Layout;
-
-type MenuItem = Required<MenuProps>['items'][number];
-
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[],
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  } as MenuItem;
-}
-
-const items: MenuItem[] = [
-  getItem('Option 1', '1', <PieChartOutlined />),
-  getItem('Option 2', '2', <DesktopOutlined />),
-  getItem('User', 'sub1', <UserOutlined />, [
-    getItem('Tom', '3'),
-    getItem('Bill', '4'),
-    getItem('Alex', '5'),
-  ]),
-  getItem('Team', 'sub2', <TeamOutlined />, [
-    getItem('Team 1', '6'),
-    getItem('Team 2', '8'),
-  ]),
-  getItem('Files', '9', <FileOutlined />),
-];
 
 const App: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -56,21 +18,14 @@ const App: React.FC = () => {
         collapsed={collapsed}
         onCollapse={(value) => setCollapsed(value)}
       >
-        <div className="demo-logo-vertical" />
-        <Menu
-          theme="dark"
-          defaultSelectedKeys={['1']}
-          mode="inline"
-          items={items}
-        />
+        <div className="flex w-full items-center justify-center p-3">
+          <img src="/logo.png" height="30px" alt="header logo" />
+        </div>
+        <Menu />
       </Sider>
       <Layout>
         <Header style={{ padding: 0, background: colorBgContainer }} />
         <Content style={{ margin: '0 16px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-          </Breadcrumb>
           <div
             style={{
               padding: 24,
---
+++ src/layouts/DefaultLayout/menu.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import {
+  DesktopOutlined,
+  UsergroupAddOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
+import { useNavigate } from 'react-router-dom';
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+export default function LayoutMenu() {
+  const navigate = useNavigate();
+
+  function getItem({
+    label,
+    key,
+    icon,
+    children,
+    url,
+  }: {
+    label: React.ReactNode;
+    key: React.Key;
+    icon?: React.ReactNode;
+    children?: MenuItem[];
+    url?: string;
+  }): MenuItem {
+    return {
+      key,
+      icon,
+      children,
+      label,
+      onClick: () => {
+        if (url) {
+          navigate(url);
+        }
+      },
+    } as MenuItem;
+  }
+
+  const menuItems: MenuItem[] = [
+    getItem({
+      label: 'Dashboard',
+      key: '1',
+      icon: <DesktopOutlined />,
+      url: '/',
+    }),
+    getItem({
+      label: 'User Manager',
+      key: 'user_manager',
+      icon: <UserOutlined />,
+      children: [
+        getItem({
+          label: 'User',
+          key: 'user_manager_user',
+          icon: <UserOutlined />,
+          url: '/user',
+        }),
+        getItem({
+          label: 'Administrator',
+          key: 'user_manager_admin',
+          icon: <UsergroupAddOutlined />,
+          url: '/user/admin',
+        }),
+      ],
+    }),
+  ];
+
+  return (
+    <Menu
+      theme="dark"
+      defaultSelectedKeys={['1']}
+      mode="inline"
+      items={menuItems}
+    />
+  );
+}
